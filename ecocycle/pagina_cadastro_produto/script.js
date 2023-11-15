c66f51ed82ba030226df3254ea45a469e0b6209e
ronaldlllima <ronald_lima@outlook.com>
2023-11-15 19:54:34 -0300

    mockup-ecocyclev1

---
+++ ecocycle/pagina_cadastro_produto/script.js
@@ -0,0 +1,15 @@
+function cadastrar() {
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var endereco = document.getElementById('endereco').value;
+    var tipoResiduo = document.getElementById('tipoResiduo').value;
+
+    // Aqui você pode fazer o que for necessário com os dados, como enviar para o servidor, etc.
+    console.log('Cadastro realizado:');
+    console.log('Nome:', nome);
+    console.log('E-mail:', email);
+    console.log('Endereço:', endereco);
+    console.log('Tipo de Resíduo:', tipoResiduo);
+
+    // Adicione aqui lógica adicional, como enviar os dados para um servidor, etc.
+}
---
+++ ecocycle/pagina_cadastro_usuario/script.js
@@ -0,0 +1,11 @@
+// Mantenha a função cadastrar() anterior aqui
+
+function loginGoogle() {
+    // Adicione aqui a lógica para autenticação via Google
+    console.log('Login com Google');
+}
+
+function loginMicrosoft() {
+    // Adicione aqui a lógica para autenticação via Microsoft
+    console.log('Login com Microsoft');
+}
